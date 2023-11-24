648724fe54001134d1e2f1c10b9d0a6975b338ec
guiireal <guii_real@hotmail.com>
2023-11-24 14:46:31 -0300

    wip

--- fundamentals/basic_types_2.dart
+++ fundamentals/basic_types_2.dart
@@ -1,3 +1,22 @@
 main() {
-  List approveds = ['Ana', 'Carlos', 'Daniel', 'Rafael'];
+  var approveds = ['Ana', 'Carlos', 'Daniel', 'Rafael'];
+
+  print(approveds is List);
+  print(approveds);
+  print(approveds.elementAt(2));
+  print(approveds[0]);
+
+  Map phones = {
+    'Ana': '+55 (11) 98765-4321',
+    'Carlos': '+55 (21) 98765-4321',
+    'Daniel': '+55 (85) 98765-4321',
+    'Rafael': '+55 (81) 98765-4321',
+  };
+
+  print(phones is Map);
+  print(phones);
+  print(phones['Ana']);
+  print(phones.keys);
+  print(phones.values);
+  print(phones.entries);
 }
