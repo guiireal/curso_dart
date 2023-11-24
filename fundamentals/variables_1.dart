64c6fcc626bb7d3c4319dba1ebc6ebe07cd52175
guiireal <guii_real@hotmail.com>
2023-11-24 12:34:46 -0300

    wip

--- fundamentals/variables_1.dart
+++ fundamentals/variables_1.dart
@@ -1,4 +1,8 @@
 main() {
   int age = 30;
+  double price = 9.99;
+
+  price = 10.99;
   print(age);
+  print(price);
 }
---
+++ fundamentals/variables_2.dart
@@ -0,0 +1,8 @@
+main() {
+  var n1 = 2;
+  var n2 = 4.10;
+  var text = 'O valor da soma é: ';
+
+  print(text + (n1 + n2).toString());
+  print(n1.runtimeType);
+}
