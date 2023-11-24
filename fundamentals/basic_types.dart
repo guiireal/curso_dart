eda52808b3d05ac73a64c0745d9c1c2063789df0
guiireal <guii_real@hotmail.com>
2023-11-24 13:07:55 -0300

    wip

--- fundamentals/basic_types.dart
+++ fundamentals/basic_types.dart
@@ -8,4 +8,30 @@ main() {
   n4 = 6.7;
 
   print(n1 + n2 + n3 + n4);
+
+  String s1 = "Bom";
+  String s2 = " dia";
+
+  print(s1 + s2.toUpperCase() + "!!!");
+
+  bool estaChovendo = true;
+
+  bool muitoFrio = false;
+
+  print(estaChovendo && muitoFrio);
+
+  dynamic x = "Um texto bem legal";
+  print(x);
+
+  x = 123;
+  print(x);
+
+  x = false;
+  print(x);
+
+  var y = "Outro texto bem legal";
+
+  // y = 123; Erro!
+
+  print(y);
 }
